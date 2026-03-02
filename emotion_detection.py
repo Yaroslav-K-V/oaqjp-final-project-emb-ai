be467450891482c6b4baf16b0e142312bd319a14
Yaroslav K <69163097+Yaroslav-K-V@users.noreply.github.com>
2026-03-03 00:32:55 +0200

    Refactor formatting

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,11 +1,16 @@
 import requests
 import json
 
+
 def emotion_detector(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    url = ('https://sn-watson-emotion.labs.skills.network/v1/'
+           'watson.runtime.nlp.v1/NlpService/EmotionPredict')
+    headers = {
+        "grpc-metadata-mm-model-id":
+        "emotion_aggregated-workflow_lang_en_stock"
+    }
     input_json = {"raw_document": {"text": text_to_analyze}}
-    
+
     response = requests.post(url, json=input_json, headers=headers)
 
     if response.status_code == 400:
@@ -17,11 +22,11 @@ def emotion_detector(text_to_analyze):
             'sadness': None,
             'dominant_emotion': None
         }
-    
+
     response_text = response.text
 
     response_dict = json.loads(response_text)
-    
+
     emotions = response_dict.get('emotionPredict', {}).get('emotion', {})
 
     anger_score = emotions.get('anger', 0)
@@ -29,7 +34,7 @@ def emotion_detector(text_to_analyze):
     fear_score = emotions.get('fear', 0)
     joy_score = emotions.get('joy', 0)
     sadness_score = emotions.get('sadness', 0)
-    
+
     emotion_scores = {
         'anger': anger_score,
         'disgust': disgust_score,
@@ -37,7 +42,7 @@ def emotion_detector(text_to_analyze):
         'joy': joy_score,
         'sadness': sadness_score
     }
-    
+
     dominant_emotion = max(emotion_scores, key=emotion_scores.get)
 
     return {
--- server.py
+++ server.py
@@ -3,9 +3,11 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
+
 
 @app.route("/emotionDetector")
 def emotion_detector_route():
@@ -15,7 +17,7 @@ def emotion_detector_route():
     result = emotion_detector(text_to_analyze)
 
     dominant_emotion = result['dominant_emotion']
-    
+
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
 
@@ -25,9 +27,14 @@ def emotion_detector_route():
     joy = result['joy']
     sadness = result['sadness']
 
-    response_text = f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
-    
+    response_text = (
+        f"For the given statement, the system response is 'anger': {anger}, "
+        f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and "
+        f"'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    )
+
     return response_text
+
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5000, debug=True)
